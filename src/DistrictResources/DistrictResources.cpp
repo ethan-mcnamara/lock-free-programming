dfe8d3ce7a58d4a0707587b1b54c104b09e415d5
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-10-16 17:59:22 -0700

    Removed unnecessary IDE-generated comments

--- src/DistrictResources/DistrictResources.cpp
+++ src/DistrictResources/DistrictResources.cpp
@@ -1,7 +1,3 @@
-//
-// Created by 17eth on 2022-10-08.
-//
-
 #include "DistrictResources.h"
 
 namespace LockFreeDispatch {
--- src/Event/Maintenance.cpp
+++ src/Event/Maintenance.cpp
@@ -1,7 +1,3 @@
-//
-// Created by 17eth on 2022-10-08.
-//
-
 #include "Maintenance.h"
 
 namespace LockFreeDispatch {
