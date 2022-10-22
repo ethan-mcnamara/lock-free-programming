79811fc9951b2d7d75371016aa12626828626689
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-10-22 15:54:17 -0700

    Changed `VehicleStatus` to `enum class`

--- src/VehicleStatus/VehicleStatus.h
+++ src/VehicleStatus/VehicleStatus.h
@@ -5,28 +5,12 @@
 #include <cstdint>
 #include <string>
 
-class VehicleStatus {
-    private:
-
-    public:
-        uint8_t status;
-
-    std::string toString() const
-    {
-        switch(status)
-        {
-            case 0:
-                return "Available";
-            case 1:
-                return "Responding";
-            case 2:
-                return "Returning";
-            case 3:
-                return "Maintenance";
-            default:
-                return "Unknown";
-        }
-    }
+enum class VehicleStatus {
+    Available,
+    Responding,
+    Returning,
+    Maintenance,
+    Unavailable
 };
 
 
