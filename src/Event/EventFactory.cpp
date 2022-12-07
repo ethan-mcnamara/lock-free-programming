5e75769ec757fcfe74adbc35a06797cd09834653
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-12-06 22:13:23 -0800

    Implement `vehiclesMeetingRequirements` function in EventFactory class

--- src/Event/EventFactory.cpp
+++ src/Event/EventFactory.cpp
@@ -1,12 +1,26 @@
 #include "EventFactory.h"
+#include <<type_traits>
 
 namespace LockFreeDispatch {
 
 
     std::vector<Vehicle> EventFactory::vehiclesMeetingRequirements(std::vector<Vehicle> *vehicleReqts,
                                                                    std::vector<Vehicle> *orderedVehicles) {
-        // TODO - implement function
-        return std::vector<Vehicle>();
+        std::vector<Vehicle> meetingRequirements;
+
+        for (Vehicle vehicle : *orderedVehicles)
+        {
+            for (Vehicle reqt : *vehicleReqts)
+            {
+                if (std::is_same<typeof(vehicle), typeof(reqt)>::value
+                    && vehicle.getCurNumCrew() >= reqt.getCurNumCrew()
+                    && vehicle.getCurWaterVolumeLitres() >= reqt.getCurWaterVolumeLitres())
+                {
+                    meetingRequirements.push_back(vehicle);
+                }
+            }
+        }
+        return meetingRequirements;
     }
 
     void EventFactory::selectVehicles(std::vector<Vehicle> *vehicleReqts, std::vector<Vehicle> *orderedVehicles) {
